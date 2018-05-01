700f330acdb8e2cb0300627497da981467403d22
Markus Faerevaag <m@frv.ag>
2018-05-02 00:26:44 +0200

    Add docstring to string helpers

--- src/Bowser/Helper.hs
+++ src/Bowser/Helper.hs
@@ -1,5 +1,16 @@
 module Bowser.Helper
-  (strip) where
+  ( strip
+  , trim
+  ) where
 
+import Data.Char (isSpace)
+
+-- |Remove quotes from ends of string
+strip :: String -> String
 strip = f . f
   where f = reverse . dropWhile (`elem` "'\"")
+
+-- |Remove whitespace from ends of string
+trim :: String -> String
+trim = f . f
+  where f = reverse . dropWhile isSpace
