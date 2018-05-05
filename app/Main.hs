0ca1bf7ea525a43919ac41c7c41fe0fb34779be1
Markus Faerevaag <m@frv.ag>
2018-05-05 16:16:11 +0200

    Add command line arguments

--- app/Main.hs
+++ app/Main.hs
@@ -1,23 +1,53 @@
 module Main where
 
+import Control.Monad
+import Data.Maybe
 import Text.Show.Pretty
+import System.Console.ParseArgs
 
 import Bowser.Parser
 import Bowser.Engine
 import Bowser.AST
 
+data Options = Filename
+             | Threshold
+             | VerboseFlag
+             deriving (Ord, Eq, Show)
+
+argd :: [ Arg Options ]
+argd = [ Arg { argIndex = Filename,
+               argName = Just "file",
+               argAbbr = Just 'f',
+               argData = argDataRequired "filename" ArgtypeString,
+               argDesc = "file to run" }
+       , Arg { argIndex = Threshold,
+               argName = Just "threshold",
+               argAbbr = Just 't',
+               argData = argDataDefaulted "default" ArgtypeInt 1000000,
+               argDesc = "step threshold for execution" }
+       , Arg { argIndex = VerboseFlag,
+               argName = Just "verbose",
+               argAbbr = Just 'v',
+               argData = Nothing,
+               argDesc = "verbose flag" }
+       ]
+
 main :: IO ()
 main = do
-  putStrLn "ast:"
-  ast <- parseFile path
-  pPrint ast
+  args <- parseArgsIO
+          (ArgsParseControl ArgsComplete ArgsHardDash)
+          argd
 
-  putStrLn ""
+  ast <- parseFile (fromJust (getArg args Filename))
 
-  putStrLn "return:"
-  res <- eval ast threshold
-  pPrint res
+  when (gotArg args VerboseFlag) $ do
+    putStrLn "ast:"
+    pPrint ast
+    putStrLn ""
 
-  where
-    path = "examples/fib_rec.js"
-    threshold = 1000000
+  res <- eval ast (toInteger ((fromJust (getArg args Threshold)::Int)))
+
+  when (gotArg args VerboseFlag) $ do
+    putStrLn ""
+    putStrLn "return:"
+    pPrint res
